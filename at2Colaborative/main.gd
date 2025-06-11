85b44432e62d9fde1ffbc30d0f15d5b74bccecf0
Peony <saulelineemoji@gmail.com>
2025-06-11 11:13:57 +0930

    it stil doesnt work lol
    it still doesnt work lol

--- at2Colaborative/main.gd
+++ at2Colaborative/main.gd
@@ -6,11 +6,15 @@ var score
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	get_tree().call_group("mobs", "queue_free")
 
 func _on_mob_timer_timeout():
 	# Create a new instance of the Mob scene.
@@ -37,8 +41,10 @@ func _on_mob_timer_timeout():
 	# Spawn the mob by adding it to the Main scene.
 	add_child(mob)
 
-func _on_score_timer_timeout() -> void:
-	pass # Replace with function body.
+func _on_score_timer_timeout():
+	score += 1
+	$HUD.update_score(score)
 
-func _on_start_timer_timeout() -> void:
-	pass # Replace with function body.
+func _on_start_timer_timeout():
+	$MobTimer.start()
+	$ScoreTimer.start()
