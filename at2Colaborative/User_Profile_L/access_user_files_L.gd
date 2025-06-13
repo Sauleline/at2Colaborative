1c2793a7067167c29da33bcb0cea94235d5b7d75
leeAnnePhie <166671876+leeAnnePhie@users.noreply.github.com>
2025-06-13 12:37:15 +0930

    User Profile Trialing

---
+++ at2Colaborative/User_Profile_L/access_user_files_L.gd
@@ -0,0 +1,26 @@
+extends Node
+
+var saveFilePath = "user://save/"
+var saveFileName = "User_Save.tres"
+
+var user1 = User.new()
+
+func _ready():
+	user1.user_name = "Test"
+	verifySaveDirectory(saveFilePath)
+	save_user(user1)
+	var user2 = load_data()
+	print(user2.user_name)
+
+func verifySaveDirectory(path : String):
+	DirAccess.make_dir_absolute(path)
+	
+func save_user(user):
+	ResourceSaver.save(user, saveFilePath+saveFileName)
+	
+
+func load_data():
+	return ResourceLoader.load(saveFilePath+saveFileName).duplicate(true)
+
+	
+	
