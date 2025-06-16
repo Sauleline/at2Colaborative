091fab32e1bf7db81de02c74e3c209d268fcf749
leeAnnePhie <166671876+leeAnnePhie@users.noreply.github.com>
2025-06-16 15:50:45 +0930

    User Profile 2

--- at2Colaborative/User_Profile_L/main.gd
+++ at2Colaborative/User_Profile_L/main.gd
@@ -1,3 +1,37 @@
-extends Node
+extends "access_user_files_L.gd"
 
-const accessUserFile = preload("res://User_Profile_L/access_user_files_L.gd")
+@onready var OptionUserSelect = $OptionSelectUser 
+@onready var UserNameInput = $UserInputName 
+
+func populate_user_list():
+	OptionUserSelect.clear()
+	for i in userProfiles.userProfilesDict.keys():
+		OptionUserSelect.add_item(i, -1)
+	
+func _ready():
+	verifySaveDirectory(saveFilePath)
+	openUserProfiles()	
+	print(userProfiles.userProfilesDict)
+	populate_user_list()
+	
+func selectUser():
+	var ID = OptionUserSelect.get_selected_id()
+	var Text = OptionUserSelect.get_item_text(ID)
+	saveUserFileName = userProfiles.userProfilesDict[Text]
+	open_user()
+	print(user.userName)
+
+func _on_select_user_pressed() -> void:
+	selectUser()
+	 # Replace with function body.
+
+func addUser():
+	var username = UserNameInput.text
+	new_user(username)
+	save_user()
+	OptionUserSelect.clear()
+	populate_user_list()
+	
+
+func _on_add_user_pressed() -> void:
+	addUser() 
