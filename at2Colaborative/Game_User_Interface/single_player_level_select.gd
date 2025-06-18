abdf560f2994d73ad4ca79e4c60da5375d154658
Peony <saulelineemoji@gmail.com>
2025-06-18 10:38:15 +0930

    delete main stage cause we don't need it anymore

--- at2Colaborative/Game_User_Interface/single_player_level_select.gd
+++ at2Colaborative/Game_User_Interface/single_player_level_select.gd
@@ -2,4 +2,4 @@ extends CanvasLayer
 
 
 func _on_tutorial_pressed() -> void:
-	get_tree().change_scene_to_file("res://Main_Game/main.tscn")
+	get_tree().change_scene_to_file("res://Main_Game/Stages/stage0.tscn")
--- at2Colaborative/Main_Game/main.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-@export var stageNum = 0
-
-func _ready():
-	var stageTo = "res://Main_Game/Stages/stage%s.tscn"%str(stageNum)
-	print(stageTo)
-	get_tree().change_scene_to_file(stageTo)
