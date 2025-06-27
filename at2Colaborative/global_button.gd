8497b83a92685180479069d53b91271979861ec2
Peony <saulelineemoji@gmail.com>
2025-06-27 13:02:55 +0930

    forgot this bit

--- at2Colaborative/global_button.gd
+++ at2Colaborative/global_button.gd
@@ -2,8 +2,9 @@ extends TextureButton
 
 @export var text: String
 @export var fontSize: int = 60
-@export var buttonColour: Color
+@export var buttonColour: Color = Color(1,1,1)
 
 func _ready():
 	$Text.text = text
 	$Text.add_theme_font_size_override("font_size", fontSize)
+	self_modulate = buttonColour
