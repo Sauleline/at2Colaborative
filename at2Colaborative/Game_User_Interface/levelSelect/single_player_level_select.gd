7b011087d3f59a21b67a539970a82bc21786acdf
leeAnnePhie <166671876+leeAnnePhie@users.noreply.github.com>
2025-07-25 11:20:17 +0930

    leaderboard

--- at2Colaborative/Game_User_Interface/levelSelect/single_player_level_select.gd
+++ at2Colaborative/Game_User_Interface/levelSelect/single_player_level_select.gd
@@ -23,7 +23,11 @@ func _ready() -> void:
 		$Grid.add_child(instance)
 		
 func sort_ascending(a, b):
-	if a[1] < b[1]:
+	if str(a[1]) == "null":
+		return false
+	elif str(b[1]) == "null":
+		return true
+	elif a[1] < b[1]:
 		return true
 	return false
 	
@@ -35,7 +39,7 @@ func _get_leaderboard(level):
 		if str(level) in user.personalBestScores.keys():
 			leaderboard.append([user.userName, user.personalBestScores[str(level)]])
 		else:
-			leaderboard.append([user.userName, 0])
+			leaderboard.append([user.userName, "null"])
 	leaderboard.sort_custom(sort_ascending)
 	return leaderboard
 		
@@ -47,12 +51,18 @@ var _populateLeaderboard = func populateLeaderboard(level: int):
 	insertLabel.text = "Level "+str(level)
 	$Leaderboard/vertContainer.add_child(insertLabel)
 	var leaderboard = _get_leaderboard(level) 
+	var count = 1
 	for i in leaderboard:
 		insertLabel = Label.new()
 		insertLabel.add_theme_font_size_override("font_size", 20)
-		var text = i[0] + " : " + str(Global.intToSecMin(floor(i[1]/10)))
+		var text = " "
+		if str(i[1]) == "null":
+			text = i[0] + " : TBC" 
+		else:
+			text = str(count) + ". " + i[0] + " : " + str(Global.intToSecMin(floor(i[1]/10)))
 		insertLabel.text = text.pad_zeros(2)
 		$Leaderboard/vertContainer.add_child(insertLabel)
+		count = count + 1
 
 var _goToLevel = func goToLevel(level: int):
 	get_tree().change_scene_to_file("res://Main_Game/1Player/Stages/"+str(level)+".tscn")
